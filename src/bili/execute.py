d7a36b10f091a325ad7a115ce92d38e701a7abb1
Lycreal <jgsly123@gmail.com>
2020-05-23 14:27:02 +0800

    修改定时任务实现方式
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- src/bili/execute.py
+++ src/bili/execute.py
@@ -1,12 +1,15 @@
 import asyncio
-from mirai import Plain, Image
+import traceback
+from mirai import Mirai, Plain, Image
 from mirai.logger import Event
 from .register import Database
 from .connection import GetDynamicStatus
 from ..app import app
 
 
-async def execute(delay: float):
+@app.subroutine
+async def execute(app: Mirai):
+    delay = 10
     while True:
         if not (hasattr(app, 'enabled') and app.enabled):
             await asyncio.sleep(delay)
@@ -19,16 +22,13 @@ async def execute(delay: float):
                     Event.info(f'动态检查：{target.name}')
                 except Exception as e:
                     Event.error(e)
+                    traceback.print_exc()
                     continue
                 if resp:
                     footer = f"\n\n本条动态的地址为: https://t.bilibili.com/{resp.dynamic_id}"
-                    Event.info(f'{target.name}动态更新：{footer}')
+                    Event.info(f'{target.name}动态更新：https://t.bilibili.com/{resp.dynamic_id}')
                     components = [Plain(resp.msg)] + \
                                  [await Image.fromRemote(url) for url in resp.imgs] + \
                                  [Plain(footer)]
                     for group_id in target.groups:
                         await app.sendGroupMessage(group=group_id, message=components)
-
-
-loop = asyncio.get_event_loop()
-loop.create_task(execute(15))
