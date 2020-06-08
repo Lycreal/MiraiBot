d33f2ddb77712fcfe1463af37ca4bc1f115c7868
Lycreal <jgsly123@gmail.com>
2020-06-08 17:15:22 +0800

    输出格式调整
    Signed-off-by: Lycreal <jgsly123@gmail.com>

--- plugins/setu/__init__.py
+++ plugins/setu/__init__.py
@@ -3,7 +3,10 @@ import random
 import re
 from typing import List, Set, Union
 
-from mirai import Mirai, Group, GroupMessage, Image, Plain
+from mirai import (
+    Mirai,
+    Group, Member, GroupMessage,
+    Image, Plain, At)
 from mirai.event.message.chain import Source
 from mirai.logger import Event as EventLogger
 
@@ -13,6 +16,8 @@ from .utils import CoolDown, shuzi2number
 cd = CoolDown(app='setu', td=20)
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
+
+LAST_QUOTA: int = 300
 
 
 @sub_app.receiver("GroupMessage")
@@ -43,14 +48,17 @@ async def checkQuota(app: Mirai, message: GroupMessage):
 
 async def setuExecutor(app: Mirai, message: GroupMessage, number: int, keyword: str):
     """根据关键词获取data_array，并调用sendSetu"""
+    global LAST_QUOTA
     member_id: int = message.sender.id
     if keyword == '':
-        if len(SetuDatabase.load_from_file().__root__) >= 300:
+        if len(SetuDatabase.load_from_file().__root__) >= 300 and LAST_QUOTA < 200:
             resp = SetuResp(code=-430, msg='空关键词')
         else:
             resp = await SetuResp.get()
+            LAST_QUOTA = resp.quota
     elif cd.check(member_id):
         resp = await SetuResp.get(keyword)
+        LAST_QUOTA = resp.quota
     else:
         resp = SetuResp(code=-3, msg='你的请求太快了，休息一下吧')
 
@@ -68,16 +76,15 @@ async def setuExecutor(app: Mirai, message: GroupMessage, number: int, keyword: 
 
 async def sendSetu(app: Mirai, message: GroupMessage, data_array: Union[Set[SetuData], List[SetuData]], number: int):
     """发送data_array"""
+    sender: Member = message.sender
     group: Group = message.sender.group
-    source: Source = message.messageChain.getSource()
 
-    async def send(_prefix: str, _data: SetuData):
+    async def send(prefix_: str, data_: SetuData):
         try:
-            setu_b: bytes = await _data.get()
+            setu_b: bytes = await data_.get()
             await app.sendGroupMessage(group,
-                                       [Plain(_prefix + _data.purl + '\n'), Image.fromBytes(setu_b)],
-                                       source)
-            EventLogger.info(f"{_prefix}色图已发送，标签：{','.join(_data.tags)}")
+                                       [At(sender.id), Plain(prefix_ + data_.purl + '\n'), Image.fromBytes(setu_b)])
+            EventLogger.info(f"{prefix_}色图已发送，标签：{','.join(data_.tags)}")
         except asyncio.TimeoutError as e:
             EventLogger.warn('连接超时' + str(e))
             raise e
@@ -87,15 +94,24 @@ async def sendSetu(app: Mirai, message: GroupMessage, data_array: Union[Set[Setu
 
     number = min(number, len(data_array))
 
+    # 延时逐个启动任务
     tasks: List[asyncio.Task] = []
     for i, data in enumerate(random.sample(data_array, k=number)):
         prefix = f'[{i + 1}/{number}]' if number > 1 else ''
         task = asyncio.create_task(send(prefix, data))
         tasks.append(task)
         await asyncio.wait(tasks, timeout=5)
+
+    # 等待返回
     done, pending = await asyncio.wait(tasks, timeout=10)
-    num_exception = len([task for task in done if task.exception()])
-    num_pending = len([t.cancel() for t in pending])
-    if num_exception or num_pending:
-        msg = f'{number}个任务中，{num_pending}个超时，{num_exception}个异常。'
-        await app.sendGroupMessage(group, msg, source)
+    num_exception = len([task.exception() for task in done if task.exception()])
+    num_timeout = len([t.cancel() for t in pending])
+
+    # 报告结果
+    if num_exception or num_timeout:
+        msg_timeout = f'{num_timeout}个任务超时' if num_timeout else ''
+        msg_exception = f'{num_exception}个任务异常' if num_exception else ''
+        msg = msg_timeout or msg_exception
+        if msg_timeout and msg_exception:
+            msg += ', ' + msg_exception
+        await app.sendGroupMessage(group, msg)
