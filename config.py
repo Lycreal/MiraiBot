6e680381b347b4975271b8dcf96dd6ba18d4cfbb
Lycreal <jgsly123@gmail.com>
2020-08-20 11:09:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,14 @@ __all__ = [
 
 data_path = os.path.join(os.path.dirname(__file__), 'data')
 
+# 在根目录下创建 config_private.py , 写入以下内容并按需修改
+# ============= START =============
+
 setu_apikey: str = ''
 setu_r18: str = '0'
 setu_proxy: str = 'disable'
+    
+# ============== END ==============
 
 try:
     from config_private import *
